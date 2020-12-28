f30f7e7b0f57372ec3a59f3cfe0070dbc880a339
rg27 <gualvezreymar@gmail.com>
2020-12-28 09:42:32 +0800

    Embedding Dashboard

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -9,11 +9,13 @@ const Dashboard = () => {
             <h2>Dashboard</h2>
               <Iframe 
               width="100%"
-              height="100%" 
-              src="https://datastudio.google.com/embed/reporting/1991e922-229e-479b-b05e-a9e87988095e/page/VgD" 
-              // frameborder="0" 
-              // style="border:0" 
-              // allowfullscreen="true"
+              height="330px" 
+              src="https://datastudio.google.com/embed/reporting/41549d3c-85be-498f-b714-9445d0a41539/page/hOauB" 
+              />
+              <Iframe 
+              width="100%"
+              height="1250px" 
+              src="https://datastudio.google.com/embed/reporting/ed31653f-3ea1-425b-a69b-e683ce81ac60/page/hOauB" 
               />
           </div>
         </div>
